dd4cca8347c7fe20da75ca68bfa9144c559884f8
bhanuc <bhanu423@gmail.com>
2017-12-03 06:06:05 +0530

    added static file

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Koa = require('koa');
-
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = require('./controller');
 const cors = require('@koa/cors');
 
 app.use(cors());
-
+app.use(serve(__dirname + '/public'));
 app
   .use(router.routes())
   .use(router.allowedMethods());
